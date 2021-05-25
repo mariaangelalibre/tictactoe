36c3df9bda5a78677a6f8a071eea06fa50862f51
Maria Angela Libre <mariaangelaplibre@iskolarngbayan.pup.edu.ph>
2021-05-26 03:53:47 +0800

    play btn try

--- tictactoe1/Form1.Designer.cs
+++ tictactoe1/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace tictactoe1
 {
-    partial class Form1
+    partial class ppppppppppppppppppppppp
     {
         /// <summary>
         ///  Required designer variable.
@@ -30,12 +30,21 @@ namespace tictactoe1
         private void InitializeComponent()
         {
             this.playbtn = new System.Windows.Forms.Button();
+            this.pplay = new System.Windows.Forms.Button();
+            this.pplay1 = new System.Windows.Forms.Button();
+            this.pplay2 = new System.Windows.Forms.Button();
+            this.pplay3 = new System.Windows.Forms.Button();
+            this.pplay6 = new System.Windows.Forms.Button();
+            this.pplay4 = new System.Windows.Forms.Button();
+            this.pplay5 = new System.Windows.Forms.Button();
+            this.pplay7 = new System.Windows.Forms.Button();
+            this.pplay8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // playbtn
             // 
             this.playbtn.Font = new System.Drawing.Font("Vineta BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.playbtn.Location = new System.Drawing.Point(163, 250);
+            this.playbtn.Location = new System.Drawing.Point(121, 312);
             this.playbtn.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
             this.playbtn.Name = "playbtn";
             this.playbtn.Size = new System.Drawing.Size(413, 60);
@@ -44,17 +53,109 @@ namespace tictactoe1
             this.playbtn.UseVisualStyleBackColor = true;
             this.playbtn.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // pplay
+            // 
+            this.pplay.Location = new System.Drawing.Point(177, 53);
+            this.pplay.Name = "pplay";
+            this.pplay.Size = new System.Drawing.Size(89, 62);
+            this.pplay.TabIndex = 1;
+            this.pplay.UseVisualStyleBackColor = true;
+            this.pplay.Visible = false;
+            this.pplay.Click += new System.EventHandler(this.pplay_Click);
+            // 
+            // pplay1
+            // 
+            this.pplay1.Location = new System.Drawing.Point(289, 53);
+            this.pplay1.Name = "pplay1";
+            this.pplay1.Size = new System.Drawing.Size(89, 62);
+            this.pplay1.TabIndex = 2;
+            this.pplay1.UseVisualStyleBackColor = true;
+            this.pplay1.Visible = false;
+            // 
+            // pplay2
+            // 
+            this.pplay2.Location = new System.Drawing.Point(401, 53);
+            this.pplay2.Name = "pplay2";
+            this.pplay2.Size = new System.Drawing.Size(89, 62);
+            this.pplay2.TabIndex = 3;
+            this.pplay2.UseVisualStyleBackColor = true;
+            this.pplay2.Visible = false;
+            // 
+            // pplay3
+            // 
+            this.pplay3.Location = new System.Drawing.Point(178, 121);
+            this.pplay3.Name = "pplay3";
+            this.pplay3.Size = new System.Drawing.Size(89, 62);
+            this.pplay3.TabIndex = 4;
+            this.pplay3.UseVisualStyleBackColor = true;
+            this.pplay3.Visible = false;
+            // 
+            // pplay6
+            // 
+            this.pplay6.Location = new System.Drawing.Point(289, 121);
+            this.pplay6.Name = "pplay6";
+            this.pplay6.Size = new System.Drawing.Size(89, 62);
+            this.pplay6.TabIndex = 7;
+            this.pplay6.UseVisualStyleBackColor = true;
+            this.pplay6.Visible = false;
+            // 
+            // pplay4
+            // 
+            this.pplay4.Location = new System.Drawing.Point(401, 121);
+            this.pplay4.Name = "pplay4";
+            this.pplay4.Size = new System.Drawing.Size(89, 62);
+            this.pplay4.TabIndex = 8;
+            this.pplay4.UseVisualStyleBackColor = true;
+            this.pplay4.Visible = false;
+            // 
+            // pplay5
+            // 
+            this.pplay5.Location = new System.Drawing.Point(178, 187);
+            this.pplay5.Name = "pplay5";
+            this.pplay5.Size = new System.Drawing.Size(89, 62);
+            this.pplay5.TabIndex = 9;
+            this.pplay5.UseVisualStyleBackColor = true;
+            this.pplay5.Visible = false;
+            // 
+            // pplay7
+            // 
+            this.pplay7.Location = new System.Drawing.Point(289, 189);
+            this.pplay7.Name = "pplay7";
+            this.pplay7.Size = new System.Drawing.Size(89, 62);
+            this.pplay7.TabIndex = 10;
+            this.pplay7.UseVisualStyleBackColor = true;
+            this.pplay7.Visible = false;
+            this.pplay7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // pplay8
+            // 
+            this.pplay8.Location = new System.Drawing.Point(401, 188);
+            this.pplay8.Name = "pplay8";
+            this.pplay8.Size = new System.Drawing.Size(89, 62);
+            this.pplay8.TabIndex = 11;
+            this.pplay8.UseVisualStyleBackColor = true;
+            this.pplay8.Visible = false;
+            // 
+            // ppppppppppppppppppppppp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(747, 416);
+            this.ClientSize = new System.Drawing.Size(573, 403);
+            this.Controls.Add(this.pplay8);
+            this.Controls.Add(this.pplay7);
+            this.Controls.Add(this.pplay5);
+            this.Controls.Add(this.pplay4);
+            this.Controls.Add(this.pplay6);
+            this.Controls.Add(this.pplay3);
+            this.Controls.Add(this.pplay2);
+            this.Controls.Add(this.pplay1);
+            this.Controls.Add(this.pplay);
             this.Controls.Add(this.playbtn);
             this.Font = new System.Drawing.Font("Vineta BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
-            this.Name = "Form1";
+            this.Name = "ppppppppppppppppppppppp";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
@@ -63,6 +164,15 @@ namespace tictactoe1
         #endregion
 
         private System.Windows.Forms.Button playbtn;
+        private System.Windows.Forms.Button pplay;
+        private System.Windows.Forms.Button pplay1;
+        private System.Windows.Forms.Button pplay2;
+        private System.Windows.Forms.Button pplay3;
+        private System.Windows.Forms.Button pplay6;
+        private System.Windows.Forms.Button pplay4;
+        private System.Windows.Forms.Button pplay5;
+        private System.Windows.Forms.Button pplay7;
+        private System.Windows.Forms.Button pplay8;
     }
 }
 
--- tictactoe1/Form1.cs
+++ tictactoe1/Form1.cs
@@ -10,19 +10,43 @@ using System.Windows.Forms;
 
 namespace tictactoe1
 {
-    public partial class Form1 : Form
+    public partial class ppppppppppppppppppppppp : Form
     {
-        public Form1()
+        public ppppppppppppppppppppppp()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            pplay.Visible = true;
+            pplay1.Visible = true;
+            pplay2.Visible = true;
+            pplay3.Visible = true;
+            pplay4.Visible = true;
+            pplay5.Visible = true;
+            pplay6.Visible = true;
+            pplay7.Visible = true;
+            pplay8.Visible = true;
+            playbtn.Visible = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pplay_Click(object sender, EventArgs e)
         {
 
         }
