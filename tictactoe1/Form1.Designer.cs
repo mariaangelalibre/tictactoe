425b6987f41c0a998ca1d61ff56128f9c9925670
Maria Angela Libre <mariaangelaplibre@iskolarngbayan.pup.edu.ph>
2021-05-26 02:51:43 +0800

    color

--- tictactoe1/Form1.Designer.cs
+++ tictactoe1/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace tictactoe1
             // playbtn
             // 
             this.playbtn.Font = new System.Drawing.Font("Vineta BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.playbtn.Location = new System.Drawing.Point(151, 226);
+            this.playbtn.Location = new System.Drawing.Point(163, 250);
             this.playbtn.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
             this.playbtn.Name = "playbtn";
             this.playbtn.Size = new System.Drawing.Size(413, 60);
@@ -48,13 +48,13 @@ namespace tictactoe1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(747, 416);
             this.Controls.Add(this.playbtn);
             this.Font = new System.Drawing.Font("Vineta BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
