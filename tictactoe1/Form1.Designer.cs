3ab444b9ed32067373fdafc4f23ea5c04147390a
Maria Angela Libre <mariaangelaplibre@iskolarngbayan.pup.edu.ph>
2021-05-27 03:28:19 +0800

    try x and o

--- tictactoe1/Form1.Designer.cs
+++ tictactoe1/Form1.Designer.cs
@@ -31,19 +31,19 @@ namespace tictactoe1
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tform));
             this.playbtn = new System.Windows.Forms.Button();
-            this.pplay = new System.Windows.Forms.Button();
-            this.pplay1 = new System.Windows.Forms.Button();
-            this.pplay2 = new System.Windows.Forms.Button();
-            this.pplay3 = new System.Windows.Forms.Button();
-            this.pplay6 = new System.Windows.Forms.Button();
-            this.pplay4 = new System.Windows.Forms.Button();
-            this.pplay5 = new System.Windows.Forms.Button();
-            this.pplay7 = new System.Windows.Forms.Button();
-            this.pplay8 = new System.Windows.Forms.Button();
+            this.xo4 = new System.Windows.Forms.Button();
+            this.xo1 = new System.Windows.Forms.Button();
+            this.xo3 = new System.Windows.Forms.Button();
+            this.xo2 = new System.Windows.Forms.Button();
+            this.xo5 = new System.Windows.Forms.Button();
+            this.xo6 = new System.Windows.Forms.Button();
+            this.xo7 = new System.Windows.Forms.Button();
+            this.xo8 = new System.Windows.Forms.Button();
+            this.xo9 = new System.Windows.Forms.Button();
             this.tictactoepic = new System.Windows.Forms.PictureBox();
-            this.plyr1 = new System.Windows.Forms.TextBox();
-            this.plyr2 = new System.Windows.Forms.TextBox();
             this.restartbtn = new System.Windows.Forms.Button();
+            this.plyr2 = new System.Windows.Forms.Label();
+            this.plyr1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.tictactoepic)).BeginInit();
             this.SuspendLayout();
             // 
@@ -59,88 +59,89 @@ namespace tictactoe1
             this.playbtn.UseVisualStyleBackColor = true;
             this.playbtn.Click += new System.EventHandler(this.button1_Click);
             // 
-            // pplay
+            // xo4
             // 
-            this.pplay.Location = new System.Drawing.Point(12, 104);
-            this.pplay.Name = "pplay";
-            this.pplay.Size = new System.Drawing.Size(102, 86);
-            this.pplay.TabIndex = 1;
-            this.pplay.UseVisualStyleBackColor = true;
-            this.pplay.Visible = false;
-            this.pplay.Click += new System.EventHandler(this.pplay_Click);
+            this.xo4.Location = new System.Drawing.Point(12, 104);
+            this.xo4.Name = "xo4";
+            this.xo4.Size = new System.Drawing.Size(102, 86);
+            this.xo4.TabIndex = 1;
+            this.xo4.UseVisualStyleBackColor = true;
+            this.xo4.Visible = false;
+            this.xo4.Click += new System.EventHandler(this.pplay_Click);
             // 
-            // pplay1
+            // xo1
             // 
-            this.pplay1.Location = new System.Drawing.Point(12, 12);
-            this.pplay1.Name = "pplay1";
-            this.pplay1.Size = new System.Drawing.Size(102, 86);
-            this.pplay1.TabIndex = 2;
-            this.pplay1.UseVisualStyleBackColor = true;
-            this.pplay1.Visible = false;
+            this.xo1.Location = new System.Drawing.Point(12, 12);
+            this.xo1.Name = "xo1";
+            this.xo1.Size = new System.Drawing.Size(102, 86);
+            this.xo1.TabIndex = 2;
+            this.xo1.UseVisualStyleBackColor = true;
+            this.xo1.Visible = false;
+            this.xo1.Click += new System.EventHandler(this.xo1_Click);
             // 
-            // pplay2
+            // xo3
             // 
-            this.pplay2.Location = new System.Drawing.Point(228, 12);
-            this.pplay2.Name = "pplay2";
-            this.pplay2.Size = new System.Drawing.Size(102, 86);
-            this.pplay2.TabIndex = 3;
-            this.pplay2.UseVisualStyleBackColor = true;
-            this.pplay2.Visible = false;
+            this.xo3.Location = new System.Drawing.Point(228, 12);
+            this.xo3.Name = "xo3";
+            this.xo3.Size = new System.Drawing.Size(102, 86);
+            this.xo3.TabIndex = 3;
+            this.xo3.UseVisualStyleBackColor = true;
+            this.xo3.Visible = false;
             // 
-            // pplay3
+            // xo2
             // 
-            this.pplay3.Location = new System.Drawing.Point(120, 12);
-            this.pplay3.Name = "pplay3";
-            this.pplay3.Size = new System.Drawing.Size(102, 86);
-            this.pplay3.TabIndex = 4;
-            this.pplay3.UseVisualStyleBackColor = true;
-            this.pplay3.Visible = false;
+            this.xo2.Location = new System.Drawing.Point(120, 12);
+            this.xo2.Name = "xo2";
+            this.xo2.Size = new System.Drawing.Size(102, 86);
+            this.xo2.TabIndex = 4;
+            this.xo2.UseVisualStyleBackColor = true;
+            this.xo2.Visible = false;
             // 
-            // pplay6
+            // xo5
             // 
-            this.pplay6.Location = new System.Drawing.Point(120, 104);
-            this.pplay6.Name = "pplay6";
-            this.pplay6.Size = new System.Drawing.Size(102, 86);
-            this.pplay6.TabIndex = 7;
-            this.pplay6.UseVisualStyleBackColor = true;
-            this.pplay6.Visible = false;
+            this.xo5.Location = new System.Drawing.Point(120, 104);
+            this.xo5.Name = "xo5";
+            this.xo5.Size = new System.Drawing.Size(102, 86);
+            this.xo5.TabIndex = 7;
+            this.xo5.UseVisualStyleBackColor = true;
+            this.xo5.Visible = false;
             // 
-            // pplay4
+            // xo6
             // 
-            this.pplay4.Location = new System.Drawing.Point(228, 104);
-            this.pplay4.Name = "pplay4";
-            this.pplay4.Size = new System.Drawing.Size(102, 86);
-            this.pplay4.TabIndex = 8;
-            this.pplay4.UseVisualStyleBackColor = true;
-            this.pplay4.Visible = false;
+            this.xo6.Location = new System.Drawing.Point(228, 104);
+            this.xo6.Name = "xo6";
+            this.xo6.Size = new System.Drawing.Size(102, 86);
+            this.xo6.TabIndex = 8;
+            this.xo6.UseVisualStyleBackColor = true;
+            this.xo6.Visible = false;
             // 
-            // pplay5
+            // xo7
             // 
-            this.pplay5.Location = new System.Drawing.Point(12, 196);
-            this.pplay5.Name = "pplay5";
-            this.pplay5.Size = new System.Drawing.Size(102, 86);
-            this.pplay5.TabIndex = 9;
-            this.pplay5.UseVisualStyleBackColor = true;
-            this.pplay5.Visible = false;
+            this.xo7.Location = new System.Drawing.Point(12, 196);
+            this.xo7.Name = "xo7";
+            this.xo7.Size = new System.Drawing.Size(102, 86);
+            this.xo7.TabIndex = 9;
+            this.xo7.UseVisualStyleBackColor = true;
+            this.xo7.Visible = false;
             // 
-            // pplay7
+            // xo8
             // 
-            this.pplay7.Location = new System.Drawing.Point(120, 196);
-            this.pplay7.Name = "pplay7";
-            this.pplay7.Size = new System.Drawing.Size(102, 86);
-            this.pplay7.TabIndex = 10;
-            this.pplay7.UseVisualStyleBackColor = true;
-            this.pplay7.Visible = false;
-            this.pplay7.Click += new System.EventHandler(this.button7_Click);
+            this.xo8.Location = new System.Drawing.Point(120, 196);
+            this.xo8.Name = "xo8";
+            this.xo8.Size = new System.Drawing.Size(102, 86);
+            this.xo8.TabIndex = 10;
+            this.xo8.UseVisualStyleBackColor = true;
+            this.xo8.Visible = false;
+            this.xo8.Click += new System.EventHandler(this.button7_Click);
             // 
-            // pplay8
+            // xo9
             // 
-            this.pplay8.Location = new System.Drawing.Point(228, 196);
-            this.pplay8.Name = "pplay8";
-            this.pplay8.Size = new System.Drawing.Size(102, 86);
-            this.pplay8.TabIndex = 11;
-            this.pplay8.UseVisualStyleBackColor = true;
-            this.pplay8.Visible = false;
+            this.xo9.Location = new System.Drawing.Point(228, 196);
+            this.xo9.Name = "xo9";
+            this.xo9.Size = new System.Drawing.Size(102, 86);
+            this.xo9.TabIndex = 11;
+            this.xo9.UseVisualStyleBackColor = true;
+            this.xo9.Visible = false;
             // 
             // tictactoepic
             // 
@@ -151,27 +152,6 @@ namespace tictactoe1
             this.tictactoepic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.tictactoepic.TabIndex = 12;
             this.tictactoepic.TabStop = false;
-            // 
-            // plyr1
-            // 
-            this.plyr1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.plyr1.Location = new System.Drawing.Point(26, 299);
-            this.plyr1.Name = "plyr1";
-            this.plyr1.Size = new System.Drawing.Size(139, 20);
-            this.plyr1.TabIndex = 13;
-            this.plyr1.Text = "PLAYER 1 -  X";
-            this.plyr1.Visible = false;
-            // 
-            // plyr2
-            // 
-            this.plyr2.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.plyr2.Location = new System.Drawing.Point(180, 299);
-            this.plyr2.Name = "plyr2";
-            this.plyr2.Size = new System.Drawing.Size(139, 20);
-            this.plyr2.TabIndex = 14;
-            this.plyr2.Text = "PLAYER 2 -  O";
-            this.plyr2.Visible = false;
-            this.plyr2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // restartbtn
             // 
@@ -185,26 +165,48 @@ namespace tictactoe1
             this.restartbtn.Visible = false;
             this.restartbtn.Click += new System.EventHandler(this.button1_Click_2);
             // 
+            // plyr2
+            // 
+            this.plyr2.AutoSize = true;
+            this.plyr2.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.plyr2.Location = new System.Drawing.Point(182, 299);
+            this.plyr2.Name = "plyr2";
+            this.plyr2.Size = new System.Drawing.Size(139, 16);
+            this.plyr2.TabIndex = 16;
+            this.plyr2.Text = "PLAYER 2 - O";
+            // 
+            // plyr1
+            // 
+            this.plyr1.AutoSize = true;
+            this.plyr1.BackColor = System.Drawing.Color.White;
+            this.plyr1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.plyr1.Location = new System.Drawing.Point(23, 299);
+            this.plyr1.Name = "plyr1";
+            this.plyr1.Size = new System.Drawing.Size(139, 16);
+            this.plyr1.TabIndex = 16;
+            this.plyr1.Text = "PLAYER 1 - X";
+            this.plyr1.Click += new System.EventHandler(this.plyr1_Click);
+            // 
             // tform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(341, 370);
-            this.Controls.Add(this.restartbtn);
-            this.Controls.Add(this.plyr2);
             this.Controls.Add(this.plyr1);
+            this.Controls.Add(this.plyr2);
+            this.Controls.Add(this.restartbtn);
             this.Controls.Add(this.playbtn);
             this.Controls.Add(this.tictactoepic);
-            this.Controls.Add(this.pplay8);
-            this.Controls.Add(this.pplay7);
-            this.Controls.Add(this.pplay5);
-            this.Controls.Add(this.pplay4);
-            this.Controls.Add(this.pplay6);
-            this.Controls.Add(this.pplay3);
-            this.Controls.Add(this.pplay2);
-            this.Controls.Add(this.pplay1);
-            this.Controls.Add(this.pplay);
+            this.Controls.Add(this.xo9);
+            this.Controls.Add(this.xo8);
+            this.Controls.Add(this.xo7);
+            this.Controls.Add(this.xo6);
+            this.Controls.Add(this.xo5);
+            this.Controls.Add(this.xo2);
+            this.Controls.Add(this.xo3);
+            this.Controls.Add(this.xo1);
+            this.Controls.Add(this.xo4);
             this.Font = new System.Drawing.Font("Vineta BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -221,19 +223,19 @@ namespace tictactoe1
         #endregion
 
         private System.Windows.Forms.Button playbtn;
-        private System.Windows.Forms.Button pplay;
-        private System.Windows.Forms.Button pplay1;
-        private System.Windows.Forms.Button pplay2;
-        private System.Windows.Forms.Button pplay3;
-        private System.Windows.Forms.Button pplay6;
-        private System.Windows.Forms.Button pplay4;
-        private System.Windows.Forms.Button pplay5;
-        private System.Windows.Forms.Button pplay7;
-        private System.Windows.Forms.Button pplay8;
+        private System.Windows.Forms.Button xo4;
+        private System.Windows.Forms.Button xo1;
+        private System.Windows.Forms.Button xo3;
+        private System.Windows.Forms.Button xo2;
+        private System.Windows.Forms.Button xo5;
+        private System.Windows.Forms.Button xo6;
+        private System.Windows.Forms.Button xo7;
+        private System.Windows.Forms.Button xo8;
+        private System.Windows.Forms.Button xo9;
         private System.Windows.Forms.PictureBox tictactoepic;
-        private System.Windows.Forms.TextBox plyr1;
-        private System.Windows.Forms.TextBox plyr2;
         private System.Windows.Forms.Button restartbtn;
+        private System.Windows.Forms.Label plyr2;
+        private System.Windows.Forms.Label plyr1;
     }
 }
 
--- tictactoe1/Form1.cs
+++ tictactoe1/Form1.cs
@@ -19,21 +19,21 @@ namespace tictactoe1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pplay.Visible = true;
-            pplay1.Visible = true;
-            pplay2.Visible = true;
-            pplay3.Visible = true;
-            pplay4.Visible = true;
-            pplay5.Visible = true;
-            pplay6.Visible = true;
-            pplay7.Visible = true;
-            pplay8.Visible = true;
+            xo4.Visible = true;
+            xo1.Visible = true;
+            xo3.Visible = true;
+            xo2.Visible = true;
+            xo6.Visible = true;
+            xo7.Visible = true;
+            xo5.Visible = true;
+            xo8.Visible = true;
+            xo9.Visible = true;
             plyr1.Visible = true;
             plyr2.Visible = true;
             restartbtn.Visible = true;
             playbtn.Visible = false;
             tictactoepic.Visible = false;
-            
+            plyr1.ForeColor = Color.Red;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -43,7 +43,14 @@ namespace tictactoe1
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-
+            if (plyr1.ForeColor == Color.Red)
+            {
+                xo1.Text = "X";
+            }
+            else
+            {
+                xo1.Text = "O";
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
@@ -53,7 +60,7 @@ namespace tictactoe1
 
         private void pplay_Click(object sender, EventArgs e)
         {
-            pplay.Text = "X";
+            xo4.Text = "X";
 
         }
 
@@ -64,6 +71,25 @@ namespace tictactoe1
 
         private void button1_Click_2(object sender, EventArgs e)
         {
+
+        }
+
+        private void plyr1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void xo1_Click(object sender, EventArgs e)
+        {
+            if (plyr1.ForeColor == Color.Red)
+            {
+                xo1.Text = "X";
+            }
+            else
+            {
+                xo1.Text = "O";
+            }
+
 
         }
     }
